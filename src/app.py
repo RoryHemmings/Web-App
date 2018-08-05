1d748a181f4677f02f64a528fae79405a961245a
RoryHemmings <rpmhemmings@gmail.com>
2018-08-05 15:31:17 -0700

    added login page

--- src/app.py
+++ src/app.py
@@ -1,14 +1,35 @@
+from common.database import Database
+from models.user import User
+
 __author__ = 'Rory'
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "urmumhavetriplegay"
 
 
 # function called if link is accessed at specified endpoint
 @app.route('/')  # www.mysite.com/api/
-def default_page_method():
+def default_page():
     return render_template('login.html')
+
+@app.before_first_request
+def initialize_database():
+    Database.initialize()
+
+
+@app.route('/login', methods=['POST'])
+def login_user():
+    email = request.form['email']
+    password = request.form['password']
+
+    if User.login_valid(email=email, password=password):
+        User.login(email)
+    else:
+        session['email'] = None
+
+    return render_template('profile.html', email=session['email'])
 
 
 if __name__ == '__main__':
