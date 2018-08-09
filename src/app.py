811179d0acf02b2e5ead2a8bddb3d1ae6809b358
RoryHemmings <rpmhemmings@gmail.com>
2018-08-09 11:06:35 -0700

    added blog system
    add blog system on the front end

--- src/app.py
+++ src/app.py
@@ -1,17 +1,18 @@
 from common.database import Database
+from models.blog import Blog
 from models.user import User
 
 __author__ = 'Rory'
 
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, make_response
 
 app = Flask(__name__)
 app.secret_key = "urmumhavetriplegay"
 
 
-# @app.route('/')
-# def home_page():
-#     render_template('home.html')
+@app.route('/')
+def home_page():
+    return render_template('home.html')
 
 
 # function called if link is accessed at specified endpoint
@@ -50,6 +51,41 @@ def register_user():
     User.register(email, password)
 
     return render_template('profile.html', email=session['email'])
+
+
+@app.route('/blogs/<string:user_id>')
+@app.route('/blogs')
+def user_blogs(user_id=None):
+    if user_id is not None:
+        user = User.get_user_by_id(user_id)
+    else:
+        user = User.get_user_by_email(session['email'])
+    blogs = user.get_blogs()
+
+    return render_template("user_blogs.html", blogs=blogs, email=user.email)
+
+
+@app.route('/blogs/new', methods=['POST', 'GET'])
+def create_new_blog():
+    if request.method == 'GET':
+        return render_template('new_blog.html')
+    else:
+        title = request.form['title']
+        description = request.form['description']
+        user = User.get_user_by_email(session['email'])
+
+        new_blog = Blog(user.email, title, description, user._id)
+        new_blog.save_to_mongo()
+
+        return make_response(user_blogs(user._id))
+
+
+@app.route('/posts/<string:blog_id>')
+def blog_posts(blog_id):
+    blog = Blog.get_from_mongo(blog_id)
+    posts = blog.get_posts()
+
+    return render_template("posts.html", posts=posts, blog_title=blog.title)
 
 
 if __name__ == '__main__':
